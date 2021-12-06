7265736a43feb1865cbd88f383e996d09eb8cf25
Patrick Kim <patrick.yrkim@gmail.com>
2021-12-05 23:54:10 -0800

    add reactive refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,20 +3,30 @@
     Home
     <p>My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
   // add composition api
   setup() {
     // will run before any other lifecycle method
-    let name = 'Daffy'
-    let age = 12
+
+    // template refs
+    // const p = ref(null)
+
+    // now reactive variables
+    const name = ref('Daffy')
+    const age = ref(12)
 
     const handleClick = () => {
-
+      name.value = 'Bugs'
+      age.value = 21
     }
 
     return {
